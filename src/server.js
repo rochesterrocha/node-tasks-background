22c9db6924528f91d87b9b567566df877e7cd7bd
Rochester Rocha <rochagondim@hotmail.com>
2021-01-17 21:50:13 -0300

    começando a construção do envio de emails

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 import 'dotenv/config'; 
 import express from 'express';
 
+import UserController from './app/controller/UserController';
+
 const app = express();
 
 app.use(express.json());
+app.post('/users', UserController.store);
+app.get('/', (req, res) => res.send(`Server running on the ${process.env.PORT}`));
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on the ${process.env.PORT}`)

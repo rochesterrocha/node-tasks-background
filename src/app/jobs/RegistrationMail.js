ad4ddde0247e30dbcbb64e5d3530cc4f49e23fd3
Rochester <rochagondim@hotmail.com>
2021-01-21 21:50:24 -0300

    configurando o redis para finalizar o projeto

--- src/app/jobs/RegistrationMail.js
+++ src/app/jobs/RegistrationMail.js
@@ -3,7 +3,8 @@ import Mail from '../lib/Mail';
 export default {
 	key: 'RegistrationMail',
 	options: {
-		//
+		delay: 5000,
+		priority: 3
 	},
 	async handle({ data }) {
 		const { user } = data;
